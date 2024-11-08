3268579a915001659c8492b5ba36a46db62a3b58
Edukaua <141458877+Edukaua@users.noreply.github.com>
2024-11-07 21:58:25 -0300

    ok

--- perguntas.js
+++ perguntas.js
@@ -1,17 +1,23 @@
 criaCartao(
     'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'O que é moto?',
+    'Moto é um meiode veiculo de transporte'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual a capital do Brasil?',
+    'A capital do Brail é Campo Largo'
 )
 
 criaCartao(
     'Programação',
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz 244 em Inglês?',
+    '244 em ingles é 244'
 )
